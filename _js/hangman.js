c0203f70e52925e2f90ed795ac10bae0b8bccb42
mischizzle <michelle@picot.ca>
2015-01-14 12:24:13 +0000

    Modified Word object into module; next step is to comvert hangman into module .. too many this references polluting internals of function

--- _js/hangman.js
+++ _js/hangman.js
@@ -1,53 +1,106 @@
 // case: need to remember past guesses; memoization??
 
+
+var dictionary = function dictionary() {
+  function getRandomWord() {
+    return "abracadabra";
+  }
+
+  return {
+    getRandomWord: getRandomWord
+  };
+};
+
+var Word = function word() {
+  var value = "";
+
+  function init() {
+    value = dictionary().getRandomWord();
+  }
+
+  function isLetterInWord(letter) {
+    return value.indexOf(letter) !== -1;
+  }
+
+  function findAllOccurencesInWord(letter) {
+    var result = [];
+    var i;
+    for (i = 0; i < value.length; i++) {
+      if (value.charAt(i) === letter) {
+        result.push(i);
+      }
+    }
+    // for (var i = 0; i < this.word.getValue().length; i++) {
+    //     if(this.word.getValue().charAt(i) === letter) {
+    //       this.answerArr[i] = letter;
+    //     }
+    //   }
+    return result;
+  }
+
+  function getValue() {
+    return value;
+  }
+
+  return {
+    init: init,
+    isLetterInWord: isLetterInWord,
+    findAllOccurencesInWord: findAllOccurencesInWord,
+    getValue: getValue
+  };
+};
+
+// var word1 = new Word();
+// // var word2 = new word();
+// word1.init();
+// console.log(word1);
+// console.log(word1.getValue());
+
 var hangman = {
 
   strikes: 0,
   maxStrikes: 9,
   guesses: 0,
   answerArr: [],
-  word: "",
+  word: new Word(),
 
-  init: function(word) {
-    this.word = word;
+  init: function init() {
+    var i;
+    this.word.init();
 
-    for(var i = 0; i < this.word.length; i++) {
+   // this seems like view layer, not domain
+    for(i = 0; i < this.word.getValue().length; i++) {
       this.answerArr[i] = "_";
     }
+    console.log(this.answerArr);
   },
 
   guess: function(letter) {
-    var letterIndex;
-
-    this.guesses++;
-
-    if(!this.checkLoss() && this.word.indexOf(letter) !== -1) {
-
-      for (var i = 0; i < this.word.length; i++) {
-        if(this.word.charAt(i) === letter) {
-          this.answerArr[i] = letter;
-        }
-      }
-      this.checkWin();
-
+    // this.guess
+    if(!this.checkLoss() && this.word.isLetterInWord(letter)) {
+      this.word.findAllOccurencesInWord(letter);
     } else {
       this.strikes++;
-      this.checkLoss();
     }
-
+    this.checkGameCompletion()
     this.logGuess(letter);
   },
 
-  checkWin: function() {
-    if(this.answerArr.join("") === this.word) {
-      this.setFinalOutcome("won");
-    }
-  },
+  // checkWin: function() {
+  //   if(this.answerArr.join("") === this.word) {
+  //     this.setFinalOutcome("won");
+  //   }
+  // },
 
   checkLoss: function() {
+    return this.strikes === this.maxStrikes;
+  },
+
+  checkGameCompletion: function() {
     if(this.strikes === this.maxStrikes) {
-      this.setFinalOutcome("lost");
-      return true;
+      console.log("lost");
+    } else {
+      console.log("won");
     }
   },
 
@@ -70,5 +123,5 @@ var hangman = {
   }
 }
 
-hangman.init("abracadabra");
+hangman.init();
 
